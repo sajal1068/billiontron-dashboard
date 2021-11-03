7263b9afd0e00dc1d6acebdf3bacac74e89c5ab5
sajal1068 <sajalkumarrewa@gmail.com>
2021-11-03 22:41:23 +0530

    3/11/2021 update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,64 +1,15 @@
-window.addEventListener("scroll", (event) => {
-    let scroll = this.scrollY;
-    console.log(scroll)
-});
-console.clear();
-const app = (() => {
-let body;
-let menu;
-let menuItems;
-
-const init = () => {
-    body = document.querySelector("body");
-    menu = document.querySelector(".menu-icon");
-    menuItems = document.querySelectorAll(".nav__list-item");
-
-    applyListeners();
-};
-
-const applyListeners = () => {
-    menu.addEventListener("click", () => toggleClass(body, "nav-active"));
-};
-
-const toggleClass = (element, stringClass) => {
-    if (element.classList.contains(stringClass))
-    element.classList.remove(stringClass);else
-    element.classList.add(stringClass);
-};
-
-init();
-})();
-
-// Navigation menu end   
-
-
-// header scroll Start 
-
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 1){  
-        $('.header').addClass("position-sticky");
+// copy text
+function CopyToClipboard(containerid) {
+    if (document.selection) {
+      var range = document.body.createTextRange();
+      range.moveToElementText(document.getElementById(containerid));
+      range.select().createTextRange();
+      document.execCommand("copy");
+    } else if (window.getSelection) {
+      var range = document.createRange();
+      range.selectNode(document.getElementById(containerid));
+      window.getSelection().addRange(range);
+      document.execCommand("copy");
+      alert("Text has been copied, now paste in the text-area")
     }
-    else{
-        $('.header').removeClass("position-sticky");
-    }
-});
-
-// accordian 
-
-$(".accordionTitle").click(function() {
-    const $accordion_wrapper = $(this).parent();
-    const $accordion_content = $(this).parent().find(".accordionContent");
-    const $accordion_open = "accordion--open";
-
-    // If this accordion is already open
-    if ($accordion_wrapper.hasClass($accordion_open)) {
-      $accordion_content.slideUp();                     // Close the content.
-      $accordion_wrapper.removeClass($accordion_open);  // Remove the accordionm--open class.
-    }
-    // If this accordion is not already open
-    else {
-      $accordion_content.slideDown();                 // Show this accordion's content.
-      $accordion_wrapper.addClass($accordion_open);   // Add the accordion--open class.
-    }
-  });
-  
+  }
